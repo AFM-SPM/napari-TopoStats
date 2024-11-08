ba13605bb6e7cbc5ba07e95714e24fd1bef3d25c
Max Gamill <mcgamill1@sheffield.ac.uk>
2024-11-08 14:50:16 +0000

    Remove removed widgets from __init__.py to make napari work

--- napari-loadafm/src/napari_topostats/__init__.py
+++ napari-loadafm/src/napari_topostats/__init__.py
@@ -6,14 +6,12 @@ except ImportError:
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
 from ._widget import (
-    ExampleQWidget,
     ImageThreshold,
     show_3d_autogenerate_widget,
     flatten,
     untilt,
     quadratic,
     nonlinear,
-    threshold_magic_widget,
 )
 from ._writer import write_multiple, write_single_image
 
@@ -22,9 +20,7 @@ __all__ = (
     "write_single_image",
     "write_multiple",
     "make_sample_data",
-    "ExampleQWidget",
     "ImageThreshold",
-    "threshold_magic_widget",
     "show_3d_autogenerate_widget",
     "flatten",
     "untilt",
