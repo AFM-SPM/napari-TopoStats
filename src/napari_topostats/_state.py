15ce66dfb1d95b9d6a586c4b5a411c9f6f122095
ahobbs7 <ahobbs834@gmail.com>
2025-11-12 13:56:19 +0000

    Improved error message pop up

--- src/napari_topostats/_state.py
+++ src/napari_topostats/_state.py
@@ -3,6 +3,11 @@ State management for napari-TopoStats.
 This module contains global state variables used for the representation of the gui across the napari-TopoStats plugin.
 """
 
+import importlib.metadata
+
 docked_widgets = []
 topostats_widget = None
 current_error_dialog = None
+
+MIN_TOPOSTATS_VERSION = "2.3.2"
+TOPOSTATS_VERSION = importlib.metadata.version("topostats")
