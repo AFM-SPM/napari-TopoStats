737b74f6125eb54259a17dc708a51569d686d111
ahobbs7 <ahobbs834@gmail.com>
2025-11-21 14:34:16 +0000

    Removed unused code in useful files

--- src/napari_topostats/__init__.py
+++ src/napari_topostats/__init__.py
@@ -5,31 +5,6 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._sample_data import make_sample_data
-from ._widget import (
-    ImageThreshold,
-    TopoStatsRootWidget,
-    gaussian_filter_image,
-    median_align_rows,
-    remove_nonlinear_background,
-    remove_planar_tilt,
-    remove_quadratic_background,
-    remove_scars_from_image,
-    zero_average_background,
-)
-from ._writer import write_multiple, write_single_image
+from ._widget import TopoStatsRootWidget
 
-__all__ = (
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ImageThreshold",
-    "remove_scars_from_image",
-    "median_align_rows",
-    "remove_planar_tilt",
-    "remove_quadratic_background",
-    "remove_nonlinear_background",
-    "zero_average_background",
-    "gaussian_filter_image",
-    "TopoStatsRootWidget",
-)
+__all__ = ("TopoStatsRootWidget",)
